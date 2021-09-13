f6a9bcaeb6ffea02daf68f5a74f622ed5f3efedc
haadfida <haad99@gmail.com>
2021-09-13 15:28:46 +0500

    Added email confirmation on Admin creation

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  protected
+
+  def after_confirmation_path_for(resource_name, resource)
+    return root_url if params[:confirmation_token].blank? || resource_class.find_by_confirmation_token(params[:confirmation_token]).blank?
+
+    # Setting reset_password_token itself to reach reset password screen for the user
+    resource = resource_class.find_by_confirmation_token(params[:confirmation_token])
+    token    = Devise.token_generator.generate(User, :reset_password_token)
+    resource.update_column(:reset_password_token, token.second)
+    resource.update_column(:reset_password_sent_at, Time.now.utc)
+    edit_user_password_url(resource, reset_password_token: token.first)
+  end
 end
