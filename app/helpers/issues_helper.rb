7db23e109d9cd9dbc25cd45e1adff4e658280471
sarooshasif1 <saroosh.asif@7vals.com>
2021-09-16 13:39:21 +0500

    Final comments resolved

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -3,9 +3,10 @@
 module IssuesHelper
   def filter_select(form, params)
     if params[:project_id].present?
-      form.select t('filter.filter_label'), Issue::FILTER.except(:Project), { include_blank: true }, { class: 'js-select-field js-filter-field form-select form-select-lg' } 
+      filters = Issue::FILTER.except(:Project)
     else
-      form.select t('filter.filter_label'), Issue::FILTER, { include_blank: true }, { class: 'js-select-field js-filter-field form-select form-select-lg' }
+      filters = Issue::FILTER
     end
+      form.select t('filter.filter_label'), filters, { include_blank: true }, { class: 'js-select-field js-filter-field form-select form-select-lg' } 
   end
 end
